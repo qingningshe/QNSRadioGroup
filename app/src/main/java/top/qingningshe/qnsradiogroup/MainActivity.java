92df7cd32c3efaebdb8e15785af609cccda4fde5
shihao <shihao>
2016-03-28 10:56:24 +0800

    增加使用方式

--- app/src/main/java/top/qingningshe/qnsradiogroup/MainActivity.java
+++ app/src/main/java/top/qingningshe/qnsradiogroup/MainActivity.java
@@ -1,7 +1,9 @@
 package top.qingningshe.qnsradiogroup;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import top.qingningshe.qnsradiogroup.library.QNSRadioGroup;
 
@@ -11,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        QNSRadioGroup radioGroup = (QNSRadioGroup) findViewById(R.id.qnsRadioGroup);
+        radioGroup.setOnCheckedChangeListener(new QNSRadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(QNSRadioGroup group, @IdRes int checkedId) {
+                Log.i("tag", checkedId + " is checked");
+            }
+        });
     }
 }
